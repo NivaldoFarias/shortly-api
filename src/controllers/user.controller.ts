2fd91555c854057e707f40916eeee97adfc04209
Nivaldo Farias <badivia.f@gmail.com>
2022-06-09 15:41:51 -0300

    feat: add users ranking endpoint default structure

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -20,6 +20,7 @@ async function getUser(_req: Request, res: Response) {
       urls.url AS "url",
       urls.views AS "visitCount"
     FROM urls 
+    GROUP BY users.id, urls.id
     LEFT JOIN users ON urls.user_id = users.id
     WHERE users.id = ?`,
     [id],
@@ -29,21 +30,40 @@ async function getUser(_req: Request, res: Response) {
 
   console.log(chalk.bold.blue(`${API} User info sent`));
   return res.status(200).send(output);
+
+  function processResult(result: any) {
+    const output = {
+      id: result[0].userId,
+      name: result[0].userName,
+      visitCount: result[0].totalVisitCount,
+      shortenedUrls: result.rows.map((row: any) => ({
+        id: row.urlId,
+        shortUrl: row.shortUrl,
+        url: row.url,
+        visitCount: row.visitCount,
+      })),
+    };
+    return { ...output };
+  }
 }
 
-function processResult(result: any) {
-  const output = {
-    id: result[0].userId,
-    name: result[0].userName,
-    visitCount: result[0].totalVisitCount,
-    shortenedUrls: result.rows.map((row: any) => ({
-      id: row.urlId,
-      shortUrl: row.shortUrl,
-      url: row.url,
-      visitCount: row.visitCount,
-    })),
-  };
-  return { ...output };
+async function getRanking(_req: Request, res: Response) {
+  const query = SqlString.format(
+    `SELECT 
+      users.id,
+      users.name,
+      COUNT(urls.id) AS "linksCount",
+      SUM(urls.views) AS "visitCount"
+    FROM users 
+    LEFT JOIN urls ON urls.user_id = users.id
+    GROUP BY users.id
+    ORDER BY "visitCount" DESC
+    LIMIT 10`,
+  );
+  const result = await client.query(query);
+
+  console.log(chalk.bold.blue(`${API} Users ranking sent`));
+  return res.status(200).send(result);
 }
 
-export { getUser };
+export { getUser, getRanking };
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
-import { getUser } from './../controllers/user.controller.js';
+import { getUser, getRanking } from './../controllers/user.controller.js';
 import requireToken from './../services/requireToken.js';
 import findUser from './../services/findUser.js';
 
 const userRouter = Router();
 
 userRouter.get(`/users/:id`, requireToken, findUser, getUser);
-//userRouter.get(`/ranking`, findUser, getRanking);
+userRouter.get(`/ranking`, findUser, getRanking);
 
 export default userRouter;
